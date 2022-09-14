076a0e9e484f7096ffd2680a1ef70ad6b74662b8
badsha <badsha@badsha.Home>
2022-09-14 22:06:51 +0500

    add pdf file

---
+++ js/tut3.js
@@ -0,0 +1,45 @@
+console.log('tut.3');
+// variable in js
+// vaar ,let .const
+var name = ` bilal`;
+var chanel;
+var marks = 3254
+console.log(name,
+    chanel,
+    marks);
+   
+   // Rules for creating JavaScript Variables
+/*
+1. Cannot start with numbers
+2. Can start with letter, numbers, _ or $
+3. Are case sensitive
+*/
+
+var city = 'pindi';
+console.log(city);
+
+const ownersName = 'bilal G';
+// ownersName = 'bilal'; // Cannot do this (error)
+console.log(ownersName);
+const fruit = 'Orange';
+    {
+        let city = 'PINDI';   
+        city = 'islamabad';
+        console.log(ci*
+
+    const arr1 = [12,23,12,53, 3];
+    // arr1.push(45);
+    console.log(arr1)
+
+
+
+
+    /* Most common programming case types:
+
+1. camelCase  
+2. kebab-case
+3. snake_case 
+4. PascalCase 
+
+
+*/
